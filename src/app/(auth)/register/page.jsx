c9a0741d3896101f03bd6fa9177c109344f7d182
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2025-01-17 08:44:50 +0800

    [dev] server action

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,26 +1,34 @@
+"use client";
+
+import { register } from "@/app/actions/auth";
 import Link from "next/link";
+import { useActionState } from "react";
 
 export default function Register() {
+  const [state, action, isPending] = useActionState(register, undefined);
+
   return (
     <div className="container w-1/2">
       <h1 className="title">Register</h1>
 
-      <form action="" className="space-y-4">
+      <form action={action} className="space-y-4">
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" />
+          <input type="text" id="email" name="email" />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" />
+          <input type="password" id="password" name="password" />
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password</label>
-          <input type="password" name="confirmPassword" />
+          <input type="password" id="confirmPassword" name="confirmPassword" />
         </div>
 
         <div className="flex items-end gap-4">
-          <button className="btn-primary">Register</button>
+          <button disabled={isPending} className="btn-primary">
+            {isPending ? "Loading..." : "Register"}
+          </button>
 
           <Link href="/" className="text-link">
             or login here
---
+++ src/app/actions/auth.js
@@ -0,0 +1,14 @@
+"use server";
+
+import { resolve } from "styled-jsx/css";
+
+export async function register(state, formData) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const confirmPassword = formData.get("confirmPassword");
+
+  console.log(email);
+  console.log(password);
+  console.log(confirmPassword);
+}
