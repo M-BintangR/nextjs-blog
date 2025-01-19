92c0d3fee9ff222597c6a1f8b97d9893ffb1d8e0
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2025-01-19 15:13:09 +0800

    feat: read blog post

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,21 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Home page</h1>
-    </div>
-  );
+import { getCollection } from "@/lib/db";
+import PostCard from "./components/PostCard";
+
+export default async function Home() {
+  const postsCollection = await getCollection("posts");
+  const posts = await postsCollection?.find().sort({ $natural: -1 }).toArray();
+
+  if (posts) {
+    return (
+      <div className="grid grid-cols-2 gap-6">
+        {posts.map((post) => (
+          <div key={post._id}>
+            <PostCard post={post} />
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <p>Failed to fetch the data from database.</p>;
+  }
 }
